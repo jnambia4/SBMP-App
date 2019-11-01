39ac8f55d9ecf9756565fd3cfb2f53aea1c0f6db
icysamur <icysamur@gmail.com>
2019-11-01 14:57:38 +0800

    Added new ClientId for Mobile Login

--- src/public/assets/js/sbmp-config.js
+++ src/public/assets/js/sbmp-config.js
@@ -2,6 +2,7 @@ var config =
 {
     "cognitoDomainURL" : "jiracorp.auth.ap-southeast-1.amazoncognito.com",
     "cognitoClientId" : "u43f2tl674qh6guik00elvini",
+    "cognitoMobileClientId" : "1sjjh5mkasng8frmrgmcpps5ai",
     "cognitoUserPoolId" : "ap-southeast-1_zDDt4ERsW",
     "cognitoTokenScopes" : ["phone", "email", "profile", "openid", "aws.cognito.signin.user.admin"],
     "sbmpFrontendURL" : "https://2qbdm0akjg.execute-api.ap-southeast-1.amazonaws.com/Prod",
@@ -15,3 +16,6 @@ var urlfiller = '?response_type=code&client_id=' + config.cognitoClientId + '&';
 var loginURL = 'https://' + config.cognitoDomainURL + '/login' + urlfiller + 'redirect_uri=' + landingURL;
 var logoutURL = 'https://' + config.cognitoDomainURL + '/logout' + urlfiller + 'logout_uri=' + frontendURL;
 var signupURL = 'https://' + config.cognitoDomainURL + '/signup' + urlfiller + 'redirect_uri=' + landingURL;
+
+var mobileurlfiller = '?response_type=code&client_id=' + config.cognitoMobileClientId + '&';
+var mobileLoginURL = 'https://' + config.cognitoDomainURL + '/login' + mobileurlfiller + 'redirect_uri=' + landingURL;
